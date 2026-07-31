dbbbb88b80a8c55db2a86ce9effe87dca2b3c5ef
chessumbrian9-web <chessumbrian9@gmail.com>
2026-07-31 11:59:26 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,21 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Change header color when scrolling
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+
+    if (window.scrollY > 50) {
+        header.style.background = "#061529";
+    } else {
+        header.style.background = "#0b1f3a";
+    }
+});
